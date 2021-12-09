48d0afaf56955602ae48a5badddef918d2f8add0
Assaf Shlomi <assaf.shlomi@broadcom.com>
2021-12-09 17:38:35 +0200

    Update newUserStd.java

--- newUserStd.java
+++ newUserStd.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main( String []args ) {
-        System.out.println( "Hello my World!!!!" );
+        System.out.println( "Hello my World!!!!!!!" );
     }
 }
 // changes for build
